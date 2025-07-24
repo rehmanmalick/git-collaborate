9152c625e68ea94fd071655ec4d8999511331fc6
rehmanmalick <abdulrehmanmalick3623@gmail.com>
2025-07-24 06:50:31 -0700

    resolved merge conflict between rehmansub1 and rehmansub1feature

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ function greet(name) {
 }
 
 console.log(greet("rehmansub1")); // Output: Hello rehmansub1
+console.log(greet("rehmansub1feature")); // Output: Hello rehmansub1feature
+
+console.log(greet("rehmansub1feature")); // Output: Hello rehmansub1feature git rebase then  rehmansub1 rebase (merge) in rehmansub1feature
