9fbe0854a17238f8ee5dc94769949a25e30a61c1
MUHAMMAD HASSAN <itsmaniwaseem5@gmail.com>
2025-07-24 18:44:22 +0500

    JS Code

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = "Hassan", age = 21;
+
+function changeGreeting() {
+  document.getElementById("greeting").innerText = 
+    age >= 18 ? "Welcome, " + name + "!" : "You must be 18+.";
+
+  for (let i = 1; i <= 3; i++) console.log("Click " + i);
+}
